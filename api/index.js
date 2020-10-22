2fe68d966d46a560dc09ac9a14c2d6265e304c2e
Frnak <francisco@glide.com>
2020-10-22 18:26:33 -0300

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,18 @@
+const TelegramBot = require("node-telegram-bot-api");
+const axios = require("axios");
+
+module.exports = async (req, res) => {
+  const bot = new TelegramBot(process.env.NODE_TELEGRAM_BOT_TOKEN, {
+    polling: false,
+  });
+  const response = await axios.get(
+    "https://www.cronista.com/MercadosOnline/json/eccheader.json"
+  );
+  await bot.sendMessage(
+    550437953,
+    `Dolar venta: ${response.data.dolarblue.valorventa},\nDolar compra: ${
+      response.data.dolarblue.valorcompra
+    },\nVariación: ${response.data.dolarblue.variacion.toFixed(2)}`
+  );
+  res.status(200).send({ ok: true });
+};
